e8ddabc4322cfd138d80e18fe5df0c977917e653
Teodor Grigor <grigor.teodor@gmail.com>
2020-10-08 19:58:29 +0100

    improved versions and activity core

--- anidero/src/main/java/com/zeoflow/anidero/OnCompositionLoadedListener.java
+++ anidero/src/main/java/com/zeoflow/anidero/OnCompositionLoadedListener.java
@@ -6,7 +6,6 @@ import androidx.annotation.Nullable;
  * @see AnideroCompositionFactory
  * @see AnideroResult
  */
-@Deprecated
 public interface OnCompositionLoadedListener {
   /**
    * Composition will be null if there was an error loading it. Check logcat for more details.
